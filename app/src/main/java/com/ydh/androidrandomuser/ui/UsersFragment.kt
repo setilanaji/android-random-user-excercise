a4db79eea022335ba65e2fe996c84eba5566af82
PC-2 <setyajiyudha299@gmail.com>
2020-11-21 14:40:00 +0700

    fix sorting Header

--- app/src/main/java/com/ydh/androidrandomuser/ui/UsersFragment.kt
+++ app/src/main/java/com/ydh/androidrandomuser/ui/UsersFragment.kt
@@ -28,7 +28,6 @@ class UsersFragment : Fragment() {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_users,
             container, false)
 
-//        setAdapter()
         setViewModel()
         setData()
         setObserver()
@@ -52,19 +51,78 @@ class UsersFragment : Fragment() {
 
         })
     }
+    data class MapList(val name: String, var index: Int)
 
     private fun generateProduct(it: List<UserModel>) {
         val list = mutableListOf<User>()
         var temp = ""
-
+        var listCategory = mutableListOf<String>()
+        var listf: List<String>
+        var index = 0
+        var countPos = 0
+        var after = 0
+        var listContainer = mutableListOf<MapList>()
         it.forEach { model ->
+//            println("now $now")
             if (temp != model.name.first[0].toUpperCase().toString()) {
                 temp = model.name.first[0].toUpperCase().toString()
 
-                list.add( User.Category(temp))
+                if (listCategory.isEmpty()){
+                    listCategory.add(temp)
+                    listf = listCategory.sorted()
+
+                    listContainer.add(index, MapList(temp, countPos))
+                    println("rencana $temp = $countPos")
+
+                    list.add(listContainer[index].index, User.Category(temp))
+//                    println(listContainer[index].name)
+//                    countPos += 1
+                }else{
+                    if ((listCategory.find { x -> x == temp }).isNullOrEmpty()){
+                        listCategory.add(temp)
+                        listf = listCategory.sorted()
+                        println(listf)
+                        for ((i, item) in listf.withIndex()){
+                            if (item == temp){
+                                index = i
+                                countPos = if (i != 0){
+                                    listContainer[i-1].index
+                                }else{
+                                    listContainer[i].index
+                                }
+                                after = countPos + 1
+
+                                println("rencana $temp = $countPos")
+                                listContainer.add(index, MapList(temp, countPos))
+                                for(k in (after) until listContainer.size){
+                                    listContainer[k].index += 1
+                                }
+                                list.add(listContainer[index].index, User.Category(temp))
+
+//                                countPos += 1
+//                                for (x in listContainer){
+//                                    if (x.name == temp){
+//                                        list.add(x.index, User.Category(temp))
+////                                        x.index = countPos
+//                                    }
+//                                }
+
+
+
+//                                println(listContainer[index].index)
+                            }
+                        }
+
+                    }
+                }
+            }
+//            list.add(listContainer[index].index , User.Data(model))
+//            now += 1
+//            listContainer[index].index = now
+            listContainer.forEach{
+                println("${it.name} ${it.index}")
             }
 
-            list.add(User.Data(model))
         }
         userAdapter?.setData(list)
 //        adapter.list = list
